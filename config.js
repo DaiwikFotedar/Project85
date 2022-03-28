4990441856c648ce775fbdf37a65e9a7420132b8
DaiwikFotedar <78803093+DaiwikFotedar@users.noreply.github.com>
2022-03-28 19:55:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyALOb9y9EGnb-6NHBKgm5tHLyzx2Xux0h4",
-    authDomain: "social-media-b49aa.firebaseapp.com",
-    projectId: "social-media-b49aa",
-    storageBucket: "social-media-b49aa.appspot.com",
-    messagingSenderId: "840110494340",
-    appId: "1:840110494340:web:9cee1a25185076a091a8d1",
-    measurementId: "G-67DGVLCTPQ"
+    apiKey: "AIzaSyDvZD6iKyCrbErnTfFGekIAx25a_es9gI8", 
+    authDomain: "spectergram-8522e.firebaseapp.com", 
+    databaseURL: "https://spectergram-8522e-default-rtdb.firebaseio.com", 
+    projectId: "spectergram-8522e", 
+    storageBucket: "spectergram-8522e.appspot.com",
+    messagingSenderId: "690072507992", 
+    appId: "1:690072507992:web:52e7ad432d62982dcf5c2d" 
 };
